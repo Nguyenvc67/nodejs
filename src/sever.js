ce543d945728806e9c5ac6797dc0bef73fe20da3
Nguyenvc67 <vucongnguyen8@gmail.com>
2023-12-10 16:51:53 +0700

    init project

--- src/sever.js
+++ src/sever.js
@@ -2,9 +2,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+afasdasda
 
-app.get('', (req, res) => {
-  res.send('Hello World!')
+app.set('views', './src/views')
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  // res.send('Hello World!')
+  res.render('sample.ejs')
 })
 
 app.get('/abc', (req, res) => {
